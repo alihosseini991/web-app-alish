417c889b7275b294159686757e9ae3ab95136655
ali-sh <alishosseini991@gmail.com>
2022-04-17 14:57:23 +0200

    test close webapp

--- main.js
+++ main.js
@@ -1,9 +1,7 @@
 const displayTelegramObject = document.querySelector(".main-button");
 
 function sendHandler() {
-    window.Telegram.WebApp.sendData({
-        test: "test 1",
-    });
+    Telegram.WebApp.close();
 }
 
 displayTelegramObject.addEventListener("click", sendHandler);

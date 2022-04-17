1e5035cfcff3f4d923eacb868fb13e3f66852716
ali-sh <alishosseini991@gmail.com>
2022-04-17 13:48:57 +0200

    handle sendData

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-const displayTelegramObject = document.querySelector(".main-input");
+const displayTelegramObject = document.querySelector(".main-button");
+
+function sendHandler() {
+    window.Telegram.WebApp.sendData({
+        test: "test 1",
+    });
+}
+
+displayTelegramObject.addEventListener("click", sendHandler);
